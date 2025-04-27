ac503a657057338c4c3622abdafe3de8b2722a58
axelviv <axelviv@live.com.ar>
2025-04-27 11:47:28 -0300

    metodos get post put y delete

---
+++ src/utils/usersUtil.js
@@ -0,0 +1,10 @@
+import { readFile, writeFile } from 'fs/promises'
+
+//usuarios
+const fileUsuarios = await readFile('./src/data/usuarios.json', 'utf-8')
+const usuarios = JSON.parse(fileUsuarios)
+
+
+export const get_user_byId = (id) =>{
+    return usuarios.find(e => e.id == id)
+}
